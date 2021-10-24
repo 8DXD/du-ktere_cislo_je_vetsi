bdc2edcf3a47609332043fb80e1227d4b49f0fff
Kateřina Dvořáková <kacule05@gmail.com>
2021-10-24 11:18:17 +0200

    Přidejte soubory projektu.

---
+++ du-ktere_cislo_je_vetsi/du-ktere_cislo_je_vetsi.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	//Porovnávání čísel
+	int x = 5, y = 6;
+	printf("Ktere cislo je vetsi? %d nebo %d?\n", x, y);
+	if (x > y) {
+		printf("  Cislo %d je vetsi nez %d\n", x, y);
+	}
+	else {
+		printf("  Cislo %d je vetsi nez %d\n", y, x);
+	} printf("\n");
+	int z = 4;
+	printf("Ktere cislo je vetsi? %d nebo %d?\n", x, z);
+	if (x > z) {
+		printf("  Cislo %d je vetsi nez %d\n", x, z);
+	}
+	else {
+		printf("  Cislo %d je vetsi nez %d\n", z, x);
+	} printf("\n");
+}
